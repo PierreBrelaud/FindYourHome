1f533025f2819c828ae4145183c15f8043292f2c
JeanMineur <PierreBrelaud@users.noreply.github.com>
2019-01-16 10:06:39 +0100

    14 - Add admin controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,12 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BackController extends AbstractController
+class AdminController extends AbstractController
 {
     /**
-    *  @Route("/admin/home", name="home_page")
-    */
-    public function iniAction()
+     *  @Route("/admin/home", name="home_page")
+     */
+    public function index()
     {
         return $this->render('back/home.html.twig', [
 
