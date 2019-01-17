a4241d2f6cb018f45acc1b050713796aab1c615d
JeanMineur <PierreBrelaud@users.noreply.github.com>
2019-01-17 13:41:10 +0100

    Update accomodation view (equipment logo) + Clean AccomodationController

--- src/Controller/AccomodationController.php
+++ src/Controller/AccomodationController.php
@@ -4,40 +4,33 @@ namespace App\Controller;
 
 
 use App\Entity\Accomodation;
-use App\Entity\Type;
-use App\Repository\TypeRepository;
-use Doctrine\Common\Persistence\ObjectManager;
-use http\Env\Response;
+use App\Repository\AccomodationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class AccomodationController extends  AbstractController {
+class AccomodationController extends AbstractController
+{
 
-    private $repository;
-    private $em;
-
-    public function __construct() {
-
-    }
-
-    public function index() {
+    public function index(AccomodationRepository $repository)
+    {
         return $this->render('front/home.html.twig', [
-            'datas' =>  $this->getDoctrine()->getRepository(Accomodation::class)->findAll()
+            'datas' => $repository->findAll()
         ]);
     }
 
-    public function view(Accomodation $accomodation) {
+    public function view(Accomodation $accomodation)
+    {
 
         //$test = $this->getDoctrine()->getRepository(Type::class)->find($accomodation->getType()->getId());
         dump($accomodation->getUser());
         return $this->render('front/view.html.twig', [
-            'accomodation'   => $accomodation,
-            'type'           => $accomodation->getType(),
-            'location'       => $accomodation->getLocation(),
-            'equipments'     => $accomodation->getEquipments()->getValues(),
+            'accomodation' => $accomodation,
+            'type' => $accomodation->getType(),
+            'location' => $accomodation->getLocation(),
+            'equipments' => $accomodation->getEquipments()->getValues(),
             'availabilities' => $accomodation->getAvalabilities()->getValues(),
-            'photos'         => $accomodation->getPhotos()->getValues(),
-            'owner'          => $accomodation->getUser(),
-            'reviews'            => $accomodation->getReviews()->getValues()
+            'photos' => $accomodation->getPhotos()->getValues(),
+            'owner' => $accomodation->getUser(),
+            'reviews' => $accomodation->getReviews()->getValues()
         ]);
     }
 }
