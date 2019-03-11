7cc13952c39bcf4ae10f783b32e1c20a10942a42
PierreBrelaud <pierrebrelaud@gmail.com>
2019-03-11 13:51:56 +0100

    Api accomodation

--- src/Controller/Api/AccomodationController.php
+++ src/Controller/Api/AccomodationController.php
@@ -5,15 +5,27 @@ namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\AccomodationRepository;
+use Symfony\Component\Asset\Packages;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 
 class AccomodationController extends AbstractController
 {
 
-    public function index(AccomodationRepository $repository)
+    public function index(AccomodationRepository $repository, Packages $assetManager)
     {
-        $response = new JsonResponse(['data' => $repository->findAll()]);
-        return $response;
+        $datas = $repository->findAll();
+
+        $path = $assetManager->getUrl('images/default/user.png');
+        //dump($path);die();
+        $response = [];
+        foreach ($datas as $data) {
+            $response[] = array(
+                'description' => $data->getDescription(),
+                'name' => $data->getName(),
+                'picture' => $path//$data->getUser()->getPicture()
+            );
+        }
+        return  new JsonResponse($response);
     }
 }
